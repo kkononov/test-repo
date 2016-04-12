ae052bc551e54bebfe4b1a149c4c3679d1cb1c29
Kirill Kononov <kirill.a.kononov@gmail.com>
2016-04-12 18:50:48 +0300

    changes in TR-CR-1

---
+++ test-java.java
@@ -0,0 +1,18 @@
+/**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Options options = new Options();
+        String publish_key = "demo-36";
+        String subscribe_key = "demo-36";
+        boolean nativ = false;
+        boolean gcm = false;
+        boolean apns = false;
+
+        String channel = "demo";
+
+        String origin = "gcm-beta";
+
+        String auth_key = "";
+
+        CommandLine cmd = null;

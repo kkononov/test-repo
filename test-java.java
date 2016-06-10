489558c91d4fcd609cf0c1c3d5987364ac3b4672
Kirill Kononov <kirill.a.kononov@gmail.com>
2016-06-10 20:55:42 +0300

    Update test-java.java

--- test-java.java
+++ test-java.java
@@ -12,6 +12,12 @@
         String channel = "demo";
 
         String origin = "gcm-beta";
+        
+        protected <T> List<T> concat(List<T> array1, List<T> array2) {
+          final ArrayList<T> res = new ArrayList<T>(array1.size() + array2.size());
+          res.addAll(array1);
+          res.addAll(array2);
+        }
 
         String auth_key = "";
 

95596e24705c98a28c73abd45107939f96525d06
Kirill Kononov <kirill.a.kononov@gmail.com>
2016-08-26 21:51:36 +0300

    Update test-java.java

--- test-java.java
+++ test-java.java
@@ -1,26 +1,9 @@
-   /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        Options options = new Options();
-        String publish_key = "demo-36";
-        String subscribe_key = "demo-36";
-        boolean nativ = false;
-        boolean gcm = false;
-        boolean apns = false;
+package Hello;
 
-        String channel = "demo";
+public class Main {
 
-        String origin = "gcm-beta";
-        // one more
-        
-        protected <T> List<T> concat(List<T> array1, List<T> array2) {
-          final ArrayList<T> res = new ArrayList<T>(array1.size() + array2.size());
-          res.addAll(array1);
-          res.addAll(array2);
-        }
+  public static void main(String[] args) {
+    String str = Klass./*TEST*/getText(10) + " is not " + Klass.getText(20) + "; ";
+  }
 
-        String auth_key = "";
-
-        CommandLine cmd = null;
-    }
+}

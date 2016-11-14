25493b454e34fe553dac7df3f09026af3bbefc66
Kirill Kononov <kirill.a.kononov@gmail.com>
2016-11-14 21:53:54 +0300

    NEWEST test three

--- test-java.java
+++ test-java.java
@@ -3,7 +3,7 @@ package Hello;
 public class Main {
 
   public static void main(String[] args) {
-    if ("1024".equals(Klass./*TEST*/getText(10) + Klass.getText(300))) {
+    if ("1024".equals(Klass./*TEST*/getText(1025) + Klass.getText(300))) {
       System.out.print("true");
     } else {
       // NOPe

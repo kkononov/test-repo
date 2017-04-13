64be77b36dfe934242e065593b28ec2bdfa2b7b4
Kirill Kononov <kirill.a.kononov@gmail.com>
2017-04-13 23:14:05 +0300

    Update test10.java

--- test10.java
+++ test10.java
@@ -2,28 +2,30 @@ package test;
 
 public class Test {
 
-  private final static String DEFAULT_COLUMN = "predictionData";
+  private final static String PREDICTION_DATA_COLUMN = "predictionData";
   private final static String NOT_MODIFIED_COLUMN = "theSame";
-  private final static String MODEL_STATE_COLUMN = "modelState";
 
-  private final Serializer<Map<String, Double>> mySerializer =
-        new MapSerializer<String, Double>(StringSerializer.Instance);
+  private final Table<String, String> myDbTable; // new commented line
+  private final Serializer<Map<String, Double>> myPredictionDataSerializer =
+    new MapSerializer<String, Double>(StringSerializer.Instance);
+
   private void function1() {
     routes.add(new StaticFileServerHandler(UpsourceConfigurationStrategy.Debug,
-        /* another comment */new File(absoluteContentRoot, "api_doc"),
-        webContext.get("/~api_doc");/* one more */
+        /* comment */new File(absoluteContentRoot, "api_doc"),
+        webContext.get("/§");/* one more */
   }
 
   @Override
   private void function2() {
-    int longNameVariable; // existing
-    /* long long long comment */
-    final String literal = "this literal IS" + "." + " IS written long";
+    int longNameVariable = 0; // new // existing
+    /* long long long */ int a; /* new comment */
+    final String literal = "this literal written long";
     final String lit;
-    lit = "literal comment literal";
+    lit = "literal and " + /* comment and */" is literal";
     /*
-     *
-     *
+     ***/int inline;
+         final String lineString;
+         float anotherInline;/***
      *
      */
   }
@@ -32,7 +34,7 @@ public class Test {
  * 2
  * 3
  * 4
- * 5 (more to remove in this block)
+ * 5 (more added in this block)
  * 6
  * 7
  */

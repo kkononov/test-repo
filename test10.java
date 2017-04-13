3edb44d1ea65f0c40ad25587255686c9317c216e
Kirill Kononov <kirill.a.kononov@gmail.com>
2017-04-13 23:13:43 +0300

    Create test10.java

---
+++ test10.java
@@ -0,0 +1,38 @@
+package test;
+
+public class Test {
+
+  private final static String DEFAULT_COLUMN = "predictionData";
+  private final static String NOT_MODIFIED_COLUMN = "theSame";
+  private final static String MODEL_STATE_COLUMN = "modelState";
+
+  private final Serializer<Map<String, Double>> mySerializer =
+        new MapSerializer<String, Double>(StringSerializer.Instance);
+  private void function1() {
+    routes.add(new StaticFileServerHandler(UpsourceConfigurationStrategy.Debug,
+        /* another comment */new File(absoluteContentRoot, "api_doc"),
+        webContext.get("/~api_doc");/* one more */
+  }
+
+  @Override
+  private void function2() {
+    int longNameVariable; // existing
+    /* long long long comment */
+    final String literal = "this literal IS" + "." + " IS written long";
+    final String lit;
+    lit = "literal comment literal";
+    /*
+     *
+     *
+     *
+     */
+  }
+}
+/* 1
+ * 2
+ * 3
+ * 4
+ * 5 (more to remove in this block)
+ * 6
+ * 7
+ */

9d1dd177141ef748094eec9d72e34eab83575bf3
Kirill Kononov <kirill.a.kononov@gmail.com>
2017-12-11 17:10:47 +0300

    Try TAT-40 again

--- test-java.java
+++ test-java.java
@@ -3,7 +3,7 @@ package Hello;
 public class Main {
 
   public static void main(String[] args) {
-    if ("1024".equals(Klass./*TEST*/getText(1025) + Klass.getText(300))) {
+    if ("1024".equals(Klass./*TEST*/getText(1024) + Klass.getText(300))) {
       System.out.print("true");
     } else {
       // NOP
